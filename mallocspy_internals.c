c64ac428b2c3a8970b523f71e2a903971a4a1439
Adrien Baur <abaur@student.42.fr>
2020-01-23 12:47:55 +0100

    Defined SpyUnreg

--- mallocspy_internals.c
+++ mallocspy_internals.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 15:38:42 by abaur             #+#    #+#             */
-/*   Updated: 2020/01/23 12:00:44 by abaur            ###   ########.fr       */
+/*   Updated: 2020/01/23 12:34:17 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,4 +69,39 @@ short	spyunreg(void *ptr)
 		ft_printf("Tried to unregister a pointer, \
 but that pointer is unknown : %#p\n", ptr);
 	return (status);
+}
+
+/*
+** @var short status
+** 	1 << 0 The pointer is registered
+** 	1 << 1 The pointer was already registered
+** 	1 << 2 Malloc failed
+*/
+
+void	*spyreg(void *ptr)
+{
+	short	status;
+	size_t	i;
+
+	status = 0;
+	i = -1;
+	while (++i < g_spycap)
+		if (g_spylist[i] == NULL && !(status & 1 << 0))
+		{
+			g_spylist[i] = ptr;
+			status |= 1 << 0;
+		}
+		else if (g_spylist == ptr)
+		{
+			if (!(status & 1 << 0))
+				g_spylist[i] = NULL;
+			status |= 1 << 1 | 1 << 0;
+		}
+	if (!(status & 1 << 0) && !spyexpand())
+		return (NULL);
+	if (!(status & 1 << 0))
+		g_spylist[i] = ptr;
+	if (SPYVERBOSE && (status & 1 << 1))
+		ft_printf("Tried to register the same pointer multiple times: %#p\n", ptr);
+	return (ptr);
 }
