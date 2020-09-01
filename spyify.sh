58b8c057e8cf7773ee3716b17f89849f369791fe
abaur <abaur@student.42.fr>
2020-09-01 18:32:27 +0200

    Removed useless shell scripts.

--- spyify.sh
+++
@@ -1,2 +0,0 @@
-perl -pi -e "s/(?<![\w_])malloc(?=\()/spymalloc/m" $(find * -name "*.c")
-perl -pi -e "s/(?<![\w_])free(?=\()/spyfree/m" $(find * -name "*.c")
--- unspyify.sh
+++
@@ -1,2 +0,0 @@
-perl -pi -e "s/(?<![\w_])spymalloc(?=\()/malloc/m" $(find * -name "*.c")
-perl -pi -e "s/(?<![\w_])spyfree(?=\()/free/m" $(find * -name "*.c")
